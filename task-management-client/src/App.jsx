bff9f3966ea9c54e9e00c142e8ce54d370e42789
suvo <suvodatta72@gamil.com>
2025-02-20 21:36:11 +0600

    task-management-client initial setup done

---
+++ task-management-client/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl text-rose-600 font-semibold">Vite + React</h1>
+
+      <p className="text-gray-200">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
